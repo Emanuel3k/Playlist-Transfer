25e7cf4dc7f5f9052567a5c73b0ba2d47b778370
emanuel3k <emanuelsantos_677@hotmail.com>
2025-04-02 17:31:48 -0300

    refactor: database connection handling to return *sql.DB and improve error propagation

--- config/postgres/config.go
+++ config/postgres/config.go
@@ -10,17 +10,17 @@ var (
 	DATABASE_CONNECTION_PATH = "DATABASE_CONNECTION_PATH"
 )
 
-func Config() error {
+func Config() (*sql.DB, error) {
 	dbConnPath := os.Getenv(DATABASE_CONNECTION_PATH)
 
 	db, err := sql.Open("postgres", dbConnPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if err := db.Ping(); err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return db, nil
 }
