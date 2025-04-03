b4d80ed3d3ccc5ff6f66d9271334395e08887a8f
emanuel3k <emanuelsantos_677@hotmail.com>
2025-04-03 18:05:08 -0300

    feat: integrate PostgreSQL configuration into user route mapping

--- cmd/http/routes/user.go
+++ cmd/http/routes/user.go
@@ -2,15 +2,21 @@ package routes
 
 import (
 	"github.com/emanuel3k/playlist-transfer/cmd/http/handlers"
+	"github.com/emanuel3k/playlist-transfer/config/postgres"
 	"github.com/emanuel3k/playlist-transfer/internal/repositories"
 	"github.com/emanuel3k/playlist-transfer/internal/services"
 	"github.com/go-chi/chi/v5"
+	"log"
 	"net/http"
 )
 
 func MapUserRoutes() http.Handler {
+	db, err := postgres.Config()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	userRepository := repositories.NewUserRepository()
+	userRepository := repositories.NewUserRepository(db)
 	userService := services.NewUserService(userRepository)
 	userHandler := handlers.NewUserHandler(userService)
 
