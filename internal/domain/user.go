cdcecce0c2e347a0f0e1f95258716b3fa0f9ba72
emanuel3k <emanuelsantos_677@hotmail.com>
2025-04-03 18:02:08 -0300

    feat: add user domain model and DTOs for user creation and response

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,7 +1,52 @@
 package domain
 
+import "github.com/emanuel3k/playlist-transfer/pkg/web"
+
+type User struct {
+	ID        *string
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+}
+
+func (u *User) ToResponse() *UserResponseDTO {
+	return &UserResponseDTO{
+		ID:        *u.ID,
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Email:     u.Email,
+	}
+}
+
+type CreateUserDTO struct {
+	FirstName string `json:"firstName" validate:"required"`
+	LastName  string `json:"lastName" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,gte=8"`
+}
+
+func (u *CreateUserDTO) ToDomain() *User {
+	return &User{
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Email:     u.Email,
+		Password:  u.Password,
+	}
+}
+
+type UserResponseDTO struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+}
+
 type UserServiceInterface interface {
+	Create(body CreateUserDTO) (*UserResponseDTO, *web.AppError)
 }
 
 type UserRepositoryInterface interface {
+	GetByEmail(email string) (*User, *web.AppError)
+	Create(user *User) *web.AppError
 }
