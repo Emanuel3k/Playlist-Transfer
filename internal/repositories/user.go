3cbd40deba318c49ae0c9b72a31106ca851b9d31
emanuel3k <emanuelsantos_677@hotmail.com>
2025-04-06 21:20:07 -0300

    feat: add UserRepository with database connection and stub methods for GetByEmail and Create

--- internal/repositories/user.go
+++ internal/repositories/user.go
@@ -1,8 +1,29 @@
 package repositories
 
+import (
+	"database/sql"
+	"github.com/emanuel3k/playlist-transfer/internal/domain"
+	"github.com/emanuel3k/playlist-transfer/pkg/web"
+)
+
 type UserRepository struct {
+	db *sql.DB
 }
 
-func NewUserRepository() *UserRepository {
-	return &UserRepository{}
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) GetByEmail(email string) (*domain.User, *web.AppError) {
+	// toodo: implement this
+
+	return nil, nil
+}
+
+func (r *UserRepository) Create(user *domain.User) *web.AppError {
+	// toodo: implement this
+
+	return nil
 }
