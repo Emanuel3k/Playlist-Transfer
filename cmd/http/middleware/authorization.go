b316687eae356781e5132a59567c569474a15701
emanuel3k <emanuelsantos_677@hotmail.com>
2025-05-04 15:38:39 -0300

    feat: implement JWT authorization middleware for secure access control

---
+++ cmd/http/middleware/authorization.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"errors"
+	"github.com/emanuel3k/playlist-transfer/pkg/web"
+	"github.com/emanuel3k/playlist-transfer/pkg/web/response"
+	"github.com/golang-jwt/jwt"
+	"net/http"
+	"os"
+	"time"
+)
+
+var invalidTokenError = web.UnauthorizedError("Invalid token")
+
+func Authorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			secret := os.Getenv("JWT_SECRET_KEY")
+			tokenValue := r.Header.Get("Authorization")
+
+			tokenValue = tokenValue[len("Bearer "):]
+
+			token, err := jwt.Parse(tokenValue, func(token *jwt.Token) (any, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, errors.New("unexpected signing method")
+				}
+
+				return []byte(secret), nil
+			})
+			if err != nil {
+				response.Send(w, http.StatusUnauthorized, invalidTokenError)
+				return
+			}
+
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok && !token.Valid {
+				response.Send(w, http.StatusUnauthorized, invalidTokenError)
+				return
+			}
+
+			if exp, ok := claims["exp"].(float64); !ok || int64(exp) < time.Now().Unix() {
+				response.Send(w, http.StatusUnauthorized, invalidTokenError)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		},
+	)
+}
