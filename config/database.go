335c569ca01f68eadaf84aca6f223c2159ae348f
emanuel3k <emanuelsantos_677@hotmail.com>
2025-05-06 20:06:33 -0300

    feat: add Redis configuration and initialization to the project

--- config/database.go
+++ config/database.go
@@ -3,11 +3,16 @@ package config
 import (
 	"fmt"
 	"github.com/emanuel3k/playlist-transfer/config/postgres"
+	"github.com/emanuel3k/playlist-transfer/config/redis"
 )
 
 func InitDB() error {
 	if err := postgres.Config(); err != nil {
 		return fmt.Errorf("error initializing postgres database: %w", err)
+	}
+
+	if err := redis.InitRedis(); err != nil {
+		return fmt.Errorf("error initializing redis database: %w", err)
 	}
 
 	return nil
